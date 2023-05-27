47f4639dc1d07d8aca23dd4c1e1d2d149b4936d2
Yuri Matvieiev <matvssv.yura@gmail.com>
2023-05-28 00:31:47 +0300

    cards for accounting expenses are ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import Costs from "./components/Costs";
 
 function App() {
+  const costs = [
+    {
+      date: new Date(2023, 2, 12),
+      description: "Refrigerator",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2023, 2, 15),
+      description: "MacBook",
+      amount: 1254.23,
+    },
+    {
+      date: new Date(2023, 2, 20),
+      description: "Book",
+      amount: 8.5,
+    },
+  ];
   return (
-   <div>
-    <h1>Hello World</h1>
-   </div>
+    <div>
+      <h1>Hello World</h1>
+      <Costs costs={costs} />
+    </div>
   );
 }
 
